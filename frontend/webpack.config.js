a3b3e5f134b5301e0d4b39ecaa97760d9e615bec
Bartłomiej Długosz <dlugosz.bartlomiej@gmail.com>
2025-06-15 11:00:07 +0200

    Add project skeleton with backend and frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    port: 3000
+  }
+};
